4e1a46a425bbea23629ae20320ffdb0b401e25c9
Alex Cipollone <alex.cipollone10@gmail.com>
2023-05-16 16:28:37 -0400

    First commit

---
+++ budget.py
@@ -0,0 +1,29 @@
+class Category
+
+  def __init__(self,ledger)
+    # if ledger != None
+    #   ledger = []
+    self.ledger = []
+
+  def deposit(self, amount, description)
+    if (description == None)
+      description == 
+    self.ledger.append({amount  amount, description description})
+    print(self.ledger)
+    return
+
+  def withdraw(self, amount, description)
+    pass
+
+  def get_balance(self)
+    pass
+
+  def transfer(self, amount, budget_category)
+    pass
+
+  def check_funds(self, amount)
+    pass
+
+
+def create_spend_chart(categories)
+  pass
